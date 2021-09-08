56a7e65abf7ff808626eb3adfa3db40fbd326a0a
matias <matifandy@gmail.com>
2021-09-08 15:16:09 -0300

    add searchbar with debounce

--- src/components/currencies/ListCurrencies.tsx
+++ src/components/currencies/ListCurrencies.tsx
@@ -1,21 +1,34 @@
-import "./ListCurrencies.css"
+import "./ListCurrencies.css";
 import { useEffect, useState } from "react";
 import { Get } from "../../services/publicApiService";
 import RowCurrency from "./RowCurrency";
-
+import useDebounce from "../../utils/usedebounce";
 
 const ListCurrencies: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [currencies, setCurrencies] = useState([]);
- 
+  
+  const debouncedSearchTerm = useDebounce(searchTerm, 1000);
+
+
   useEffect(() => {
-    Get("/currencies").then(res => setCurrencies(res));
+    Get("/currencies").then((res) => setCurrencies(res));
   }, []);
+
   return (
     <div className="ListCurrencies">
+      <div className="searchbar">
+          <input
+          className="searchbar__input"
+            type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search by name"
+          />
+        </div>
       <table className="ListCurrencies__table">
         <thead className="ListCurrencies__thead">
           <tr className="ListCurrencies__tr">
-          <th className="ListCurrencies__tr"></th>
+            <th className="ListCurrencies__tr"></th>
             <th className="ListCurrencies__tr">*</th>
             <th className="ListCurrencies__tr">Name</th>
             <th className="ListCurrencies__tr">Price</th>
@@ -26,7 +39,8 @@ const ListCurrencies: React.FC = () => {
             <th className="ListCurrencies__tr">Circulating Supply</th>
           </tr>
         </thead>
-            <RowCurrency currencies={currencies}/>
+        
+        <RowCurrency currencies={currencies} searchTerm={debouncedSearchTerm} />
       </table>
     </div>
   );
--- src/components/currencies/RowCurrency.tsx
+++ src/components/currencies/RowCurrency.tsx
@@ -1,15 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Currency } from "../../types/types";
 import "./RowCurrency.css"
 
 interface Props {
     currencies: Currency[];
+    searchTerm: any;
   }
 
-const RowCurrency: React.FC<Props> = ({currencies}) => {
+const RowCurrency: React.FC<Props> = ({currencies,searchTerm}) => { 
+ const filterCurrencies = (currencies:any, searchTerm:any) => {
+  if (!searchTerm) {
+      return currencies;
+  }
+  return currencies.filter((currency:any) => {
+      const currencyName = currency.name.toLowerCase();
+      return currencyName.includes(searchTerm);
+  });
+};
+const filteredCurrencies = filterCurrencies(currencies, searchTerm);
+
   return (
+  
     <tbody>
-      {currencies.map((currency:Currency) => (
+      {filteredCurrencies.map((currency:Currency) => (
         <tr key={currency.id}>
           <th></th>
           <th>{currency.cmc_rank}</th>
