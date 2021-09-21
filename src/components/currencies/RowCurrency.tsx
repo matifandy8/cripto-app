04df6c70599f651c27723d6edcb349164858b264
matias <matifandy@gmail.com>
2021-09-20 21:53:32 -0300

    styles dropdown

--- src/components/currencies/RowCurrency.tsx
+++ src/components/currencies/RowCurrency.tsx
@@ -1,37 +1,48 @@
 import { useEffect, useState } from "react";
 import { Currency } from "../../types/types";
-import "./RowCurrency.css"
+import "./RowCurrency.css";
 
 interface Props {
-    currencies: Currency[];
-    searchTerm: string;
-  }
+  currencies: Currency[];
+  searchTerm: string;
+}
 
-const RowCurrency: React.FC<Props> = ({currencies,searchTerm}) => { 
-  const styles = { green: { color: '#16c784' }, red: { color: '#ff0000' } }
-
- const filterCurrencies = (currencies:Currency[], searchTerm:string) => {
-  if (!searchTerm) {
+const RowCurrency: React.FC<Props> = ({ currencies, searchTerm }) => {
+  const filterCurrencies = (currencies: Currency[], searchTerm: string) => {
+    if (!searchTerm) {
       return currencies;
-  }
-  return currencies.filter((currency:Currency) => {
+    }
+    return currencies.filter((currency: Currency) => {
       const currencyName = currency.name.toLowerCase();
       return currencyName.includes(searchTerm);
-  });
-};
-const filteredCurrencies = filterCurrencies(currencies, searchTerm);
-
+    });
+  };
+  const filteredCurrencies = filterCurrencies(currencies, searchTerm);
+  const Red = {
+    color: "red",
+  } as React.CSSProperties;
+  const Green = {
+    color: "greem",
+  } as React.CSSProperties;
+  const getColor = (num: number) => {
+    return num > 0 ? Green : Red;
+  };
   return (
-  
     <tbody>
-      {filteredCurrencies.map((currency:Currency) => (
+      {filteredCurrencies.map((currency: Currency) => (
         <tr key={currency.id}>
           <th></th>
           <th>{currency.cmc_rank}</th>
-          <th>{currency.name}    {currency.symbol}</th>
+          <th>
+            {currency.name} {currency.symbol}
+          </th>
           <th>${currency.quote.USD.price.toLocaleString()}</th>
-          <th style={currency.quote.percent_change_24h < 0 ? styles.red : styles.green }>{currency.quote.USD.percent_change_24h.toFixed(2)}%</th>
-          <th style={ currency.quote.percent_change_7d < 0 ? styles.red : styles.green} >{currency.quote.USD.percent_change_7d.toFixed(2)}%</th>
+          <th style={getColor(currency.quote.percent_change_24h)}>
+            {currency.quote.USD.percent_change_24h.toFixed(2)}%
+          </th>
+          <th style={getColor(currency.quote.percent_change_7d)}>
+            {currency.quote.USD.percent_change_7d.toFixed(2)}%
+          </th>
           <th>{currency.quote.USD.market_cap.toLocaleString()}</th>
           <th>{currency.quote.USD.volume_24h.toLocaleString()}%</th>
           <th>{currency.circulating_supply.toLocaleString()}</th>
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,54 +2,63 @@ import { useState } from "react";
 import ModalLogin from "../auth/login/ModalLogin";
 import ModalRegister from "../auth/register/ModalRegister";
 import "./navbar.css";
-import { Link, useHistory } from 'react-router-dom'
-import user from "../../assets/user.png"
-
+import { Link, useHistory } from "react-router-dom";
+import user from "../../assets/profile.png";
 
 const Navbar: React.FC = () => {
   const [registerModalOpen, setRegisterModalOpen] = useState(false);
   const [loginModalOpen, setLoginModalOpen] = useState(false);
-  const history = useHistory()
+  const history = useHistory();
 
-  
   const handleLogoutClick = () => {
-    localStorage.removeItem('token')
-    history.push('/')
-}
+    localStorage.removeItem("token");
+    history.push("/");
+  };
 
   const authButton = () => {
-    if (localStorage.getItem('token') === null) {
-        return (
-            <div>
-               <button className="login" onClick={() => {
-          setLoginModalOpen(true);
-        }}>Log In</button>
-            <button className="signin openModalBtn"
-        onClick={() => {
-          setRegisterModalOpen(true);
-        }}>Sign Up</button>
-            </div>
-        )
-            
+    if (localStorage.getItem("token") === null) {
+      return (
+        <div>
+          <button
+            className="login"
+            onClick={() => {
+              setLoginModalOpen(true);
+            }}
+          >
+            Log In
+          </button>
+          <button
+            className="signin openModalBtn"
+            onClick={() => {
+              setRegisterModalOpen(true);
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
+      );
     } else {
-        return (
+      return (
         <div className="dropdown">
-          <img className="dropbtn" src={user}/>
-    <div className="dropdown-content">
-      <div className="dropdown-profile">
-    <img className="img-profile" src={user}/>
-     <p className="text-profile">View my profile</p>
-     </div>
-       <a href="#">Watchlist</a>
-       <a href="#">Portfolio</a>
-       <a href="#">Account Settings</a>       
-      <button  onClick={handleLogoutClick}>Logout</button>
-    </div>
-</div>
-        )
+          <img className="dropbtn" src={user} />
+          <div className="dropdown-content">
+            <div className="dropdown-profile">
+              <Link to="/settings">
+                <img className="img-profile" src={user} />
+                <p className="text-profile">View my profile</p>
+              </Link>
+            </div>
+            <Link to="/watchlist">Watchlist</Link>
+            <Link to="/portfolio ">Portfolio</Link>
+            <Link to="/settings">Account Settings</Link>
+            <button className="logout" onClick={handleLogoutClick}>
+              Log out
+            </button>
+          </div>
+        </div>
+      );
     }
-}
-
+  };
 
   return (
     <div className="navbar">
@@ -57,7 +66,7 @@ const Navbar: React.FC = () => {
       <div className="navbar__links">
         <ul>
           <li>
-            <a href="/Cryptocurrencies">Cryptocurrencies</a>
+            <a href="/">Cryptocurrencies</a>
           </li>
           <li>
             <a href="/exchanges">Exchanges</a>
@@ -70,12 +79,11 @@ const Navbar: React.FC = () => {
           </li>
         </ul>
       </div>
-      <div className="navbar__auth">
-      {authButton()}
-      </div>
-       {registerModalOpen && <ModalRegister setOpenModal={setRegisterModalOpen} />}
-       {loginModalOpen && <ModalLogin setOpenModal={setLoginModalOpen} />}
-
+      <div className="navbar__auth">{authButton()}</div>
+      {registerModalOpen && (
+        <ModalRegister setOpenModal={setRegisterModalOpen} />
+      )}
+      {loginModalOpen && <ModalLogin setOpenModal={setLoginModalOpen} />}
     </div>
   );
 };
